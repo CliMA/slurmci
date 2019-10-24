a228fc139eb03c37aba577f905c13eadcf58e8dc
Simon Byrne <simonbyrne@gmail.com>
2019-10-24 10:03:26 -0700

    only request 1 gpu / test

--- scripts/test-gpu-tests.sh
+++ scripts/test-gpu-tests.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=1:00:00     # walltime
 #SBATCH --nodes=1          # number of nodes
 #SBATCH --mem-per-cpu=4G   # memory per CPU core
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:1
 
 set -euo pipefail
 set -x #echo on
