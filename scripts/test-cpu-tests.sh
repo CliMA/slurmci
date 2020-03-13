6228f60a12592df942e72efff863e7942ce0acd3
K Pamnany <kpamnany@gmail.com>
2020-03-13 09:34:19 -0700

    Boost times for `test/runtests*` again.

--- scripts/test-cpu-tests.sh
+++ scripts/test-cpu-tests.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-#SBATCH --time=1:30:00     # walltime
+#SBATCH --time=2:00:00     # walltime
 #SBATCH --nodes=1          # number of nodes
+#SBATCH --ntasks=4         # number of cores
 #SBATCH --mem-per-cpu=4G   # memory per CPU core
 
 set -euo pipefail
@@ -10,6 +11,7 @@ set -x #echo on
 cd ${CI_SRCDIR}
 
 export JULIA_DEPOT_PATH="$(pwd)/.slurmdepot/cpu"
+export JULIA_NUM_THREADS=4
 export OPENBLAS_NUM_THREADS=1
 export UCX_WARN_UNUSED_ENV_VARS=n
 export CLIMA_GPU=false
