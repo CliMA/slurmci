c698dd5b2d547a008ef98ac96b6c9e78c70f6c6c
kpamnany <kpamnany@users.noreply.github.com>
2020-04-17 12:31:47 -0400

    Switch from OpenMPI 4.0.1 to OpenMPI 4.0.3 (#21)

--- scripts/test-gpu-tests.sh
+++ scripts/test-gpu-tests.sh
@@ -15,6 +15,6 @@ export OPENBLAS_NUM_THREADS=1
 export UCX_ERROR_SIGNALS="SIGILL,SIGBUS,SIGFPE" # SIGSEGV is used by Julia
 export PATH="/groups/esm/common/julia-1.3:/usr/sbin:$PATH"
 
-module load cuda/10.0 openmpi/4.0.1_cuda-10.0
+module load cuda/10.0 openmpi/4.0.3_cuda-10.0
 
 julia --color=no --project test/runtests_gpu.jl
