3096bc39e3045f5ed5fe80be873d2a5275060ce4
K Pamnany <kpamnany@gmail.com>
2020-05-14 10:42:09 -0700

    Move `pkg"precompile"` to a separate Julia session

--- scripts/test-gpu-init.sh
+++ scripts/test-gpu-init.sh
@@ -19,6 +19,7 @@ export PATH="/groups/esm/common/julia-1.3:/usr/sbin:$PATH"
 
 module load cuda/10.0 openmpi/4.0.3_cuda-10.0 hdf5/1.10.1 netcdf-c/4.6.1
 
-julia --color=no --project -e 'using Pkg; pkg"instantiate"; pkg"build"; pkg"precompile"'
+julia --color=no --project -e 'using Pkg; pkg"instantiate"; pkg"build MPI"'
+julia --color=no --project -e 'using Pkg; pkg"precompile"'
 
 cat Manifest.toml
