c3231d4c67dd6844e55107cac1397ba807a39da0
Simon Byrne <simonbyrne@gmail.com>
2020-05-15 10:28:35 -0700

    Use verbose build, remove unneeded UCX vars

--- scripts/perf-gpu-init.sh
+++ scripts/perf-gpu-init.sh
@@ -14,10 +14,11 @@ cd ${CI_SRCDIR}
 export JULIA_DEPOT_PATH="$(pwd)/.slurmdepot/gpu"
 export JULIA_MPI_BINARY=system
 export OPENBLAS_NUM_THREADS=1
-export UCX_ERROR_SIGNALS="SIGILL,SIGBUS,SIGFPE" # SIGSEGV is used by Julia
 export PATH="/groups/esm/common/julia-1.3:/usr/sbin:$PATH"
 
 module load cuda/10.0 openmpi/4.0.3_cuda-10.0 hdf5/1.10.1 netcdf-c/4.6.1
 
-julia --color=no --project -e 'using Pkg; pkg"instantiate"; pkg"build MPI"'
-julia --color=no --project -e 'using Pkg; pkg"precompile"'
+julia --color=no --project -e 'using Pkg; Pkg.instantiate(); Pkg.build(;verbose=true")'
+julia --color=no --project -e 'using Pkg; Pkg.precompile()'
+
+cat Manifest.toml
