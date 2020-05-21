a5ec30a79a91d132a4524af96a26556d07ebf8b5
Simon Byrne <simonbyrne@gmail.com>
2020-05-21 13:01:27 -0700

    fix relative paths

--- cron.sh
+++ cron.sh
@@ -4,9 +4,11 @@ if [[ "$HOSTNAME" != "login1" ]]; then
 fi
 
 source /etc/bashrc
-if [ -f "env.sh" ]; then
+
+ENV_SCRIPT="$(dirname "$0")/env.sh"
+if [ -f "$ENV_SCRIPT" ]; then
     # for setting SBATCH_ and SLURMCI_ vars
-    source env.sh
+    source "$ENV_SCRIPT"
 fi
 module load julia/1.4.1
 cd "$(dirname "$0")"
