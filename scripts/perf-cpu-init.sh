9a3658820bf99dfe431c3cdc6ded44cfb9e54ea1
Simon Byrne <simonbyrne@gmail.com>
2020-05-29 10:56:07 -0700

    Switch to Julia 1.4 (#36)
    * update to julia 1.4
    
    * disable cpu runtests

--- scripts/perf-cpu-init.sh
+++ scripts/perf-cpu-init.sh
@@ -13,9 +13,8 @@ cd ${CI_SRCDIR}
 export JULIA_DEPOT_PATH="$(pwd)/.slurmdepot/cpu"
 export JULIA_MPI_BINARY=system
 export OPENBLAS_NUM_THREADS=1
-export PATH="/groups/esm/common/julia-1.3:/usr/sbin:$PATH"
 
-module load openmpi/4.0.1 hdf5/1.10.1 netcdf-c/4.6.1
+module load openmpi/4.0.3 julia/1.4.1 hdf5/1.10.1 netcdf-c/4.6.1
 
 julia --color=no --project -e 'using Pkg; Pkg.instantiate(); Pkg.build(;verbose=true)'
 julia --color=no --project -e 'using Pkg; Pkg.precompile()'
--- scripts/test-gpu-tests.sh
+++ scripts/test-gpu-tests.sh
@@ -15,7 +15,6 @@ export JULIA_DEPOT_PATH="$(pwd)/.slurmdepot/gpu"
 export OPENBLAS_NUM_THREADS=1
 
 module purge
-export PATH="/groups/esm/common/julia-1.3:/usr/sbin:$PATH"
-module load cuda/10.0 openmpi/4.0.3_cuda-10.0 hdf5/1.10.1 netcdf-c/4.6.1
+module load julia/1.4.1 cuda/10.0 openmpi/4.0.3_cuda-10.0 hdf5/1.10.1 netcdf-c/4.6.1
 
 julia --color=no --project test/runtests_gpu.jl
