568e6f21f5ed129f5016dc4605abdccc3303fc48
Simon Byrne <simonbyrne@gmail.com>
2020-06-02 13:30:38 -0700

    use Julia 1.4.2 (#37)

--- cron.sh
+++ cron.sh
@@ -10,6 +10,6 @@ if [ -f "$ENV_SCRIPT" ]; then
     # for setting SBATCH_ and SLURMCI_ vars
     source "$ENV_SCRIPT"
 fi
-module load julia/1.4.1
+module load julia/1.4.2
 cd "$(dirname "$0")"
 julia --project slurmci.jl "$@" &>> "log/$(date +\%Y-\%m-\%d)"
--- scripts/perf-gpu-init.sh
+++ scripts/perf-gpu-init.sh
@@ -15,7 +15,7 @@ export JULIA_DEPOT_PATH="$(pwd)/.slurmdepot/gpu"
 export JULIA_MPI_BINARY=system
 export OPENBLAS_NUM_THREADS=1
 
-module load cuda/10.0 openmpi/4.0.3_cuda-10.0 julia/1.4.1 hdf5/1.10.1 netcdf-c/4.6.1
+module load cuda/10.0 openmpi/4.0.3_cuda-10.0 julia/1.4.2 hdf5/1.10.1 netcdf-c/4.6.1
 
 julia --color=no --project -e 'using Pkg; Pkg.instantiate(); Pkg.build(;verbose=true)'
 julia --color=no --project -e 'using Pkg; Pkg.precompile()'
