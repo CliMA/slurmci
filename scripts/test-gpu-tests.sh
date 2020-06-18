ab6ae0cc2a3903091b19b20cab61987250729783
Jake Bolewski <jakebolewski@gmail.com>
2020-06-18 09:54:55 -0600

    add fallback fix_rng_seed env varaible setting for deterministic tests behavior (#43)

--- scripts/test-gpu-tests.sh
+++ scripts/test-gpu-tests.sh
@@ -14,6 +14,7 @@ cd ${CI_SRCDIR}
 export JULIA_DEPOT_PATH="$(pwd)/.slurmdepot_gpu"
 export JULIA_CUDA_USE_BINARYBUILDER=false
 export OPENBLAS_NUM_THREADS=1
+export CLIMATEMACHINE_SETTINGS_FIX_RNG_SEED=true
 
 module purge
 module load julia/1.4.2 cuda/10.0 openmpi/4.0.3_cuda-10.0 hdf5/1.10.1 netcdf-c/4.6.1
